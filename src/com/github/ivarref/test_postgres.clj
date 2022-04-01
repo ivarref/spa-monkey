0742d026b185ad5c56de2e427389bfbf8d045872
Ivar Refsdal <refsdal.ivar@gmail.com>
2022-04-01 10:16:22 +0200

    Use tufte for p99 etc

--- src/com/github/ivarref/test_postgres.clj
+++ src/com/github/ivarref/test_postgres.clj
@@ -1,5 +1,6 @@
 (ns com.github.ivarref.test-postgres
-  (:require [com.github.ivarref.spa-monkey :as spa])
+  (:require [com.github.ivarref.spa-monkey :as spa]
+            [taoensso.tufte :as tufte])
   (:import (org.apache.tomcat.jdbc.pool PoolProperties DataSourceProxy)
            (java.sql Connection)))
 
@@ -8,8 +9,7 @@
 
 (def jdbc-password (System/getenv "POSTGRES_PASSWORD"))
 
-#_(def uri (str "jdbc:postgresql://127.0.0.1:5432/postgres?user=postgres&password=" jdbc-password))
-(def uri (str "jdbc:postgresql://127.0.0.1:54320/postgres?user=postgres&password=" jdbc-password))
+(def uri (str "jdbc:postgresql://127.0.0.1:5432/postgres?user=postgres&password=" jdbc-password))
 
 (defn create-datasource! [uri]
   (spa/start! st)
@@ -44,7 +44,7 @@
 (defn get-conn [^DataSourceProxy ds]
   (let [start-time (System/nanoTime)]
     (with-open [^Connection conn (.getConnection ds)]
-      (let [valid? (.isValid conn 250)
+      (let [valid? (tufte/p "isValid" (.isValid conn 250))
             spent-time (double (/ (- (System/nanoTime) start-time) 1e6))]
         (assert valid?)
         #_(println "Got connection in" (format "%.1f" spent-time) "ms")
@@ -54,12 +54,17 @@
 ; direct: => {0 1000}
 
 (defn test-ds []
-  (let [ds (create-datasource! uri)]
+  (let [ds (create-datasource! uri)
+        n 100]
     (try
-      (into (sorted-map)
-            (frequencies (loop [res []]
-                           (if (= (count res) 1000)
-                             res
-                             (recur (conj res (get-conn ds)))))))
+      (let [[res stats] (tufte/profiled
+                          {}
+                          (into (sorted-map)
+                                (frequencies (loop [res []]
+                                               (if (= (count res) n)
+                                                 res
+                                                 (recur (conj res (get-conn ds))))))))]
+        (println (tufte/format-pstats @stats))
+        res)
       (finally
         (close! ds)))))
