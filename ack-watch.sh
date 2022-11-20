16a0659feff5f61753db627de1801e83a0b046ad
Ivar Refsdal <refsdal.ivar@gmail.com>
2022-11-20 13:48:16 +0100

    ...

--- ack-watch.sh
+++ ack-watch.sh
@@ -11,7 +11,7 @@ trap 'trap - SIGTERM && export BREAK="true"; kill -- -$$;' SIGINT SIGTERM EXIT
 # https://jvns.ca/blog/2020/06/28/entr/
 while [[ "${BREAK}" == "false" ]]
 do
-  { git ls-files; git ls-files . --exclude-standard --others; } | grep -v "logs/\|^\." | entr -dz clojure -X:break-after-ack
+  { git ls-files; git ls-files . --exclude-standard --others; } | grep -v "logs/\|^\." | entr -d clojure -X:break-after-ack
   sleep 3
   if [[ "${BREAK}" == "false" ]]
   then
