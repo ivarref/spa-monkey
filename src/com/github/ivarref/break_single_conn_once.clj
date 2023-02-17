6089230d7ee972c74173d55c166306851f556a2c
Ivar Refsdal <refsdal.ivar@gmail.com>
2023-02-17 22:55:03 +0100

    ...

--- src/com/github/ivarref/break_single_conn_once.clj
+++ src/com/github/ivarref/break_single_conn_once.clj
@@ -11,7 +11,9 @@
     [taoensso.timbre :as timbre])
   (:import
     (com.github.ivarref GetSockOpt)
-    (java.net Socket)
+    (java.io BufferedReader InputStreamReader PrintWriter)
+    (java.net InetSocketAddress ServerSocket Socket)
+    (java.nio.charset StandardCharsets)
     (java.sql Connection)
     (javax.sql PooledConnection)
     (org.postgresql.core PGStream QueryExecutor QueryExecutorBase)
@@ -144,40 +146,99 @@
              {}
              state))
 
-(defn watch-socket! [^Socket sock]
+(defn watch-socket! [nam ^Socket sock]
   (future
     (try
       (let [initial-state (get-state sock)
             fd (GetSockOpt/getFd sock)]
-        (log/info "Initial state for fd" fd initial-state)
+        (log/info nam "Initial state for fd" fd initial-state)
         (loop [prev-state (with-clock initial-state (System/currentTimeMillis))]
           ;(Thread/sleep 1)
           (let [now-ms (System/currentTimeMillis)
                 {:strs [open?] :as new-state} (get-state sock)]
-             (if (not= new-state (no-clock prev-state))
-               (do
-                 (doseq [[new-k new-v] new-state]
-                   (when (and (not= new-v (first (get prev-state new-k)))
-                              (not (contains? #{"tcpi_last_ack_recv"
-                                                "tcpi_last_ack_sent"
-                                                "tcpi_last_data_recv"
-                                                "tcpi_last_data_sent"}
-                                              new-k)))
-                     (let [ms-diff (- now-ms (second (get prev-state new-k)))]
-                       (log/info "fd" fd new-k (first (get prev-state new-k)) "=>" new-v (str "(In " ms-diff " ms)")))))
-                 (when open?
-                   (recur (reduce-kv (fn [o k [old-v _old-ms :as old-val]]
-                                        (if (not= old-v (get new-state k))
-                                          (assoc o k [(get new-state k) now-ms])
-                                          (assoc o k old-val)))
-                                     {}
-                                     prev-state))))
-               (when open?
-                 (recur prev-state))))))
+            (if (not= new-state (no-clock prev-state))
+              (do
+                (doseq [[new-k new-v] new-state]
+                  (when (and (not= new-v (first (get prev-state new-k)))
+                             (not (contains? #{"tcpi_last_ack_recv"
+                                               "tcpi_last_ack_sent"
+                                               "tcpi_last_data_recv"
+                                               "tcpi_last_data_sent"}
+                                             new-k)))
+                    (let [ms-diff (- now-ms (second (get prev-state new-k)))]
+                      (log/info nam "fd" fd new-k (first (get prev-state new-k)) "=>" new-v (str "(In " ms-diff " ms)")))))
+                (when open?
+                  (recur (reduce-kv (fn [o k [old-v _old-ms :as old-val]]
+                                      (if (not= old-v (get new-state k))
+                                        (assoc o k [(get new-state k) now-ms])
+                                        (assoc o k old-val)))
+                                    {}
+                                    prev-state))))
+              (when open?
+                (recur prev-state))))))
       (catch Throwable t
         (if (.isClosed sock)
-          (log/warn "Error in socket watcher for fd" (GetSockOpt/getFd sock) ", message:" (ex-message t))
-          (log/error "Error in socket watcher for fd" (GetSockOpt/getFd sock) ", message:" (ex-message t)))))))
+          (log/warn nam "Error in socket watcher for fd" (GetSockOpt/getFd sock) ", message:" (ex-message t))
+          (log/error nam "Error in socket watcher for fd" (GetSockOpt/getFd sock) ", message:" (ex-message t)))))))
+
+(defn handle-client [running? ^Socket sock]
+  (try
+    (with-open [in (-> sock
+                       .getInputStream
+                       (InputStreamReader. StandardCharsets/UTF_8)
+                       BufferedReader.)
+                out (PrintWriter. (.getOutputStream sock) true StandardCharsets/UTF_8)]
+      (loop []
+        (when-let [line (.readLine in)]
+          (log/info "Server received:" line)
+          (Thread/sleep 1000)
+          (.println out line)
+          (Thread/sleep 1000)
+          (when @running? (recur)))))
+    (finally
+      (.close sock))))
+
+(defn accept-loop [running? server-socket]
+  (try
+    (when @running?
+      (log/info "Waiting for connections ..."))
+    (while @running?
+      (let [new-client (.accept server-socket)]
+        (watch-socket! "server" new-client)
+        (future (handle-client running? new-client))))
+    (log/info "Server exiting")
+    (catch Throwable t
+      (log/error "Unexpected error:" (ex-message t)))))
+
+(defn getsockopt-demo [_]
+  (try
+    (log-init/init-logging! {:levels [[#{"datomic.*"} :warn]
+                                      [#{"com.github.ivarref.*"} :info]
+                                      [#{"*"} :info]]})
+    (let [tcp-retry-file "/proc/sys/net/ipv4/tcp_retries2"]
+      (log/info tcp-retry-file "is" (str/trim (slurp tcp-retry-file))))
+    (let [ss (doto (ServerSocket.)
+               (.setReuseAddress true)
+               (.bind (InetSocketAddress. "localhost" 8080)))
+          port (.getLocalPort ss)
+          running? (atom true)]
+      (log/info "Listening at port" port)
+      (future (accept-loop running? ss))
+      (Thread/sleep 100)
+      (with-open [sock (Socket. "localhost" ^int port)
+                  out (PrintWriter. (.getOutputStream sock) true StandardCharsets/UTF_8)
+                  in (-> sock
+                         .getInputStream
+                         (InputStreamReader. StandardCharsets/UTF_8)
+                         BufferedReader.)]
+        (watch-socket! "client" sock)
+        (.println out "Hello world!")
+        (log/info "client sent Hello world!")
+        (when-let [line (.readLine in)]
+          (log/info "client received:" line)))
+      #_(let [client ()]))
+    (catch Throwable t
+      (log/error t "Unexpected exception:" (ex-message t)))))
 
 (defn do-test! [{:keys [block?] :as opts}]
   (try
@@ -232,7 +293,7 @@
           (log/info "Query on blocked connection ... Done in" (log-init/ms->duration (- stop-time start-time))
                     "aka" (int (- stop-time start-time)) "milliseconds")
           (log/info "Waiting 90 seconds for datomic.process-monitor")
-          (Thread/sleep 90000)))) ; Give datomic time to report StorageGetMsec
+          (Thread/sleep 90000))))                           ; Give datomic time to report StorageGetMsec
     (when block?
       @(promise))
     (catch Throwable t
