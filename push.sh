5a6cfe018062abe6e92e418f1dff931c1eec50e8
Ivar Refsdal <refsdal.ivar@gmail.com>
2024-04-01 12:50:39 +0200

    Launch hugo with file watcher

--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+trap "kill 0" SIGINT
+
+(cd $HOME/code/quickstart; hugo server -D --disableFastRender &)
+
 printf network-failures.md | entr cp -fv ./network-failures.md ~/code/quickstart/content/posts/network-failures.md
