591547982d620746c94a2addbd8900575838ca19
Ivar Refsdal <refsdal.ivar@gmail.com>
2024-04-02 13:05:05 +0200

    ...

--- blog-server.sh
+++ blog-server.sh
@@ -2,6 +2,6 @@
 
 trap "kill 0" SIGINT
 
-(cd $HOME/code/quickstart; hugo server -D --disableFastRender &)
+(cd $HOME/code/quickstart && hugo server -D --disableFastRender &)
 
 printf network-failures.md | entr cp -fv ./network-failures.md ~/code/quickstart/content/posts/network-failures.md
